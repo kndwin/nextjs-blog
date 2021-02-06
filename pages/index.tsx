9f384591cdd8903df256fc4868ede03c854180f1
Kevin Nguyen <me@kndwin.dev>
2021-02-07 03:31:07 +1100

    🔨 added type for allPostDate in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,12 @@ import { getSortedPostsData } from 'lib/posts'
 import Link from 'next/link'
 
 export default function Home({ allPostsData, profile  }: {
+	allPostsData: {
+		date: string
+		title: string
+		id: string
+
+	}[],
   profile: any,
 }) {
   const tags = {
